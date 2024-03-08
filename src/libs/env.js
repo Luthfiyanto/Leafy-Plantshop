b1bd2326009a0280d110772a5623e003047a9879
Luthfiyanto <luthfiyantooo@students.unnes.ac.id>
2024-03-08 15:28:42 +0700

    add utils and config database

--- src/libs/env.js
+++ src/libs/env.js
@@ -8,7 +8,8 @@ if (process.env.NODE_ENV !== "production") {
 
   const envPath = `.env.${isLocalEnvExists ? "local" : "development"}`;
 
-  console.info(`Loading environtment variables from ${envPath}`);
+  // eslint-disable-next-line no-console
+  console.info(`Loading environment variables from ${envPath}`);
 
   dotenv.config({
     path: envPath,
@@ -17,10 +18,10 @@ if (process.env.NODE_ENV !== "production") {
 
 const { HEROKU_APP_NAME, HEROKU_PR_NUMBER } = process.env;
 
-export const PUBLIC_URL = HEROKU_APP_NAME ? `https://${HEROKU_APP_NAME}.herokuapp.com` : /**@type {string} */ (process.env.PUBLIC_URL);
+export const PUBLIC_URL = HEROKU_APP_NAME ? `https://${HEROKU_APP_NAME}.herokuapp.com` : /** @type {string} */ (process.env.PUBLIC_URL);
 
-export const DB_DATABASE = HEROKU_PR_NUMBER ? `PR_${HEROKU_PR_NUMBER}` : process.env.DB_DATABASE;
+export const DB_DATABASE = HEROKU_PR_NUMBER ? `pr_${HEROKU_PR_NUMBER}` : process.env.DB_DATABASE;
 
 export const HOST_PORT = process.env.PORT ?? process.env.HOST_PORT;
 
-export const { DB_HOST, DB_PORT, JWT_SECRET, DB_USERNAME, DB_PASSWORD, FRONTEND_URL, EMAIL_ADDRESS, EMAIL_PASSWORD, CLOUDINARY_API_KEY, CLOUDINARY_CLOUD_NAME, CLOUDINARY_API_SECRET } = process.env;
+export const { DB_HOST, DB_PORT, JWT_SECRET, DB_USERNAME, DB_PASSWORD, FRONTEND_URL, EMAIL_ADDRESS, EMAIL_PASSWORD, CLOUDINARY_API_KEY, CLOUDINARY_CLOUD_NAME, CLOUDINARY_API_SECRET } = /** @type {Record<string, string>} */ (process.env);
